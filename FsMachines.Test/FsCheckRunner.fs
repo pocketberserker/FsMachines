69991261f456e8cef193e4cbee4f7b6be2b76128
pocketberserker <pocketberserker@gmail.com>
2013-03-21 08:41:07 +0900

    add FsCheck Runner

---
+++ FsMachines.Test/FsCheckRunner.fs
@@ -0,0 +1,20 @@
+﻿module FsCheck.NUnit
+
+open FsCheck
+open NUnit.Framework
+
+let private nunitRunner =
+    { new IRunner with
+        member x.OnStartFixture t = ()
+        member x.OnArguments(ntest, args, every) = ()
+        member x.OnShrink(args, everyShrink) = ()
+        member x.OnFinished(name, result) = 
+            match result with 
+            | TestResult.True data -> 
+                printfn "%s" (Runner.onFinishedToString name result)
+            | _ -> Assert.Fail(Runner.onFinishedToString name result) }
+   
+let private nunitConfig = { Config.Default with Runner = nunitRunner }
+
+let fsCheck name testable =
+    FsCheck.Check.One (name, nunitConfig, testable)
