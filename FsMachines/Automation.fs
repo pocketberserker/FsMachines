b17293fa4556edee6c4b5234fcefbde7f7fd8d8b
pocketberserker <pocketberserker@gmail.com>
2013-03-21 08:36:52 +0900

    Add process_ function to Automation module.

---
+++ FsMachines/Automation.fs
@@ -0,0 +1,8 @@
+﻿module Machines.Automation
+
+open Plan
+
+let rec process_ = function
+  | Mealy step -> bind (fun i -> match step i with | (o, next) -> emit o >>. process_ next) await
+  | Moore(msg, next) -> emit msg >>. bind (fun i -> process_ (next i)) await
+ 
