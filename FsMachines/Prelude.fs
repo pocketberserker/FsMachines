1955cc8db355e59fbc0cbbb22367f21ad1f06481
pocketberserker <pocketberserker@gmail.com>
2013-03-23 02:32:09 +0900

    add Seq.foldBack function.

---
+++ FsMachines/Prelude.fs
@@ -0,0 +1,16 @@
+﻿namespace Machines
+
+[<AutoOpen>]
+module Seq =
+
+  let (|EmptySeq|Cons|) xs =
+    if xs |> Seq.isEmpty then EmptySeq
+    else Cons ((Seq.head xs), (Seq.skip 1 xs))
+
+  let rec foldBack f xs init =
+    let xs = Seq.cache xs
+    match xs with
+    | EmptySeq -> init
+    | Cons(x, xs) -> f x (lazy foldBack f xs init)
+
+  let cons x (xs: Lazy<'a seq>) = seq { yield x; yield! xs.Force() }
