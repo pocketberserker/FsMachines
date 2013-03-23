3785e3a041bf446bb9b9cdadc3bd4508b3e5f4e2
pocketberserker <pocketberserker@gmail.com>
2013-03-23 15:51:02 +0900

    add Source type and functions to Source module.

---
+++ FsMachines/Source.fs
@@ -0,0 +1,15 @@
+﻿module Machines.Source
+
+open Plan
+
+type Source<'k, 'o> = Machine<'k, 'o>
+
+let repeated o : Source<'k, 'o> = o |> emit |> repeatedly
+
+let cap (r : Source<'k, 'i>) (p : Process<'i, 'o>) : Source<'k, 'o> = r |> andThen p
+
+let cycled foldBack os : Source<'k, 'o> =
+  traversePlan_ foldBack os emit |>repeatedly
+
+let source os : Source<'k, 'o> =
+  traversePlan_ foldBack os emit |> compile
