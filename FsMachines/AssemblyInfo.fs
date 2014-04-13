ff6b8fddcacaa433d6b491d903a185e8db11c036
pocketberserker <pocketberserker@gmail.com>
2014-04-13 18:39:08 +0900

    add configuration to create nuget package

---
+++ FsMachines/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("F# Machines")>]
+[<assembly: AssemblyDescription("FsMachines is a stream processing library for F#.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FsMachines")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("D58F0F07-3684-4A66-8A40-598D1ED8ABE4")>]
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+
+()
---
+++ Package.bat
@@ -0,0 +1,1 @@
+.nuget\nuget.exe pack FsMachines\FsMachines.fsproj -Symbols -Build -Prop Configuration=Release
